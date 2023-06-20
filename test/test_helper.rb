62f1349c4217ba1ccc35eafd72ed8db4c2c381d1
Mario Alberto Chávez <mario.chavez@gmail.com>
2023-06-20 13:37:45 -0600

    Adds RubyNext support in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+ENV["RUBY_NEXT_TRANSPILE_MODE"] = "rewrite"
+ENV["RUBY_NEXT_EDGE"] = "1"
+ENV["RUBY_NEXT_PROPOSED"] = "1"
+require "ruby-next/language/runtime" unless ENV["CI"]
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "llm_client"
 
